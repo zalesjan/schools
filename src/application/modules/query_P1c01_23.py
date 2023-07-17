14a83581515b9fcbb77d3d796b3018aaeda67715
Jan Zalesak <zalesjan@gmail.com>
2023-07-17 12:52:02 +0200

    making that module, last function is successfully using pivot_table function in pandas to show how many people are in given salary stages and classes

--- src/application/modules/query_P1c01_23.py
+++ src/application/modules/query_P1c01_23.py
@@ -20,7 +20,8 @@ def prepare_for_P1c01_23(bucket_name, file_name, job, department, stupen, trida,
 
 
 
-def P1c01_23_count_people_by_department_stupen_trida_NO_PIVOT(bucket_name, file_name, department, show_result=False):
+def P1c01_23_count_people_by_department_stupen_trida_COUNT(bucket_name, file_name, department, show_result=False):
+    
     # Query the file
     df = pd.read_csv(f"s3://{bucket_name}/{file_name}")
 
@@ -43,11 +44,11 @@ def P1c01_23_count_people_by_department_stupen_trida_NO_PIVOT(bucket_name, file_
             # Count the number of people in the current 'platova trida'
             count = len(trida_df)
 
-            # Append the count result to the DataFrame
+            #Append the count result to the DataFrame
             count_result = pd.concat([count_result, pd.DataFrame({'department': [department], 'stupen': [stupen], 'trida': [trida], 'count': [count]})], ignore_index=True)
 
     if show_result:
-        # Display the count result
+    # Display the count result
         if not count_result.empty:
             st.write("Count Results:")
             st.write(count_result)
@@ -58,13 +59,17 @@ def P1c01_23_count_people_by_department_stupen_trida_NO_PIVOT(bucket_name, file_
 
 
 
-def P1c01_23_count_people_first_PIVOT(bucket_name, file_name, department, show_result=False):
+def P1c01_23_count_people_by_department_stupen_trida_PIVOT_and_COUNT(bucket_name, file_name, department, show_result=False):
     # Query the file
     df = pd.read_csv(f"s3://{bucket_name}/{file_name}")
     
     # Filter the data for the specified department
     filtered_data = df[df['department'] == department]
-    
+
+    # Make the counting of people who are in individual stupen and trida
+    P1c01_23_count_people_by_department_stupen_trida_COUNT(bucket_name, file_name, department, show_result=False)
+
+
     # Pivot the data to get separate columns for each platova trida
     pivot_table = filtered_data.pivot_table(index=['department', 'stupen'], columns='trida', values='count', aggfunc='sum', fill_value=0)
     
@@ -79,7 +84,7 @@ def P1c01_23_count_people_first_PIVOT(bucket_name, file_name, department, show_r
     return pivot_table
 
 
-def P1c01_23_count_people_by_department_stupen_trida(bucket_name, file_name, department, show_result=False):
+def P1c01_23_count_people_by_department_stupen_trida_PIVOT(bucket_name, file_name, department, show_result=False):
     # Query the file
     df = pd.read_csv(f"s3://{bucket_name}/{file_name}")
     
@@ -97,4 +102,63 @@ def P1c01_23_count_people_by_department_stupen_trida(bucket_name, file_name, dep
         else:
             st.warning("No matching records found.")
     
+    return pivot_table
+
+
+
+
+
+
+
+def P1c01_23_count_people_by_department_stupen_trida(bucket_name, file_name, department, show_result=False):
+    # Query the file
+    df = pd.read_csv(f"s3://{bucket_name}/{file_name}")
+    
+    # Filter the data for the specified department
+    filtered_data = df[df['department'] == department]
+    
+    # Pivot the data to get separate columns for each platova trida
+    pivot_table = filtered_data.pivot_table(index=['department', 'stupen'], columns='trida', values='count', aggfunc='sum', fill_value=0)
+    
+    # Add a column for the total count
+    pivot_table['Total'] = pivot_table.sum(axis=1)
+    
+    if show_result:
+        # Display the pivot table
+        if not pivot_table.empty:
+            st.write("Count Results:")
+            st.write(pivot_table)
+        else:
+            st.warning("No matching records found.")
+    
+    return pivot_table
+
+
+
+
+
+def P1c01_23_count_people_by_department_stupen_trida_COUNTING(bucket_name, file_name, department, show_result=False):
+    # Query the file
+    df = pd.read_csv(f"s3://{bucket_name}/{file_name}")
+
+    # Filter data for the specified department
+    department_df = df[df['department'] == department]
+
+    # Group by 'stupen' and 'trida' and calculate the count
+    count_result = department_df.groupby(['department', 'stupen', 'trida']).size().reset_index(name='count')
+
+    # Create the pivot table to rearrange the 'trida' values as columns
+    pivot_table = count_result.pivot_table(index=['department', 'stupen'], columns='trida', values='count', aggfunc='sum', fill_value=0)
+
+    # Reset the index to convert the 'stupen' back to a regular column
+    pivot_table = pivot_table.reset_index()
+
+    if show_result:
+        # Display the count result
+        if not pivot_table.empty:
+            st.write("Count Results:")
+            st.write(pivot_table)
+        else:
+            st.warning("No matching records found.")
+
     return pivot_table
