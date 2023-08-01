191d82720b949112d6aafa8a81aa2d679a2fd783
Jan Zalesak <zalesjan@gmail.com>
2023-08-01 21:56:33 +0200

    added new function to query someone by the director

--- src/application/modules/query_file.py
+++ src/application/modules/query_file.py
@@ -17,6 +17,21 @@ def query_file(bucket_name, file_name, name, first_name, show_result=False):
     # Return the query result
     return query_result
 
+def query_someone(bucket_name, file_name, looked_name, looked_first_name, show_result=False):
+    # Query the file
+    df = pd.read_csv(f"s3://{bucket_name}/{file_name}")
+    query_result = df[(df['Name'] == looked_name) & (df['FirstName'] == looked_first_name)]
+    
+    if show_result:
+        # Display query result
+        if not query_result.empty:
+            st.write("Query Results:")
+            st.write(query_result)
+        else:
+            st.warning("No matching records found.")
+    
+    # Return the query result
+    return query_result
 
 def show_certain_columns(bucket_name, file_name, name, first_name, show_result=False):
     # Query the file
