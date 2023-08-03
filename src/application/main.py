8edcd5c7a50614c1fcc9175dc09d203e3dbb9949
Jan Zalesak <zalesjan@gmail.com>
2023-08-03 18:48:26 +0200

    still cannot fix the code validation issue. The code is always generated anew. So when user is given the chance to insert the code, it already generates a new one

--- src/application/main.py
+++ src/application/main.py
@@ -1,11 +1,12 @@
 import streamlit as st
 from modules.file_operations_S3 import check_file_exists, upload_file
-from modules.query_file import query_file, show_certain_columns
+from modules.query_file import query_file, show_certain_columns, query_someone
 from modules.query_form_P1c1_2023 import P1c01_23_count_people_by_department_stupen_trida
 from modules.send_confirm_email import send_confirmation_email
-from modules.validate_code import validate_code
+from modules.validate_code import validate_code, split_names
 
 def main():
+    sent_code = None
     department = 'operation'
     bucket_name = "schoolworkhours"
     entered_code = None
@@ -47,39 +48,55 @@ def main():
             st.warning("No matching records found.")
             return  # Exit the function if no matching records are found
 
-        # Generate the verification code (school name + random 2-digit number)
         import random
-        code = f"{school_name}_{random.randint(10, 99)}"
+        sent_code = f"{school_name}_{random.randint(10, 99)}"
 
         # Send the confirmation email with the code
         if st.button("Pošli potvrzovací mail. / Send me confirmation email"):
-            send_confirmation_email(email, code)
+            # Generate the verification code (school name + random 2-digit number)
+            send_confirmation_email(email, sent_code)
 
         # Prompt for the code
         entered_code = st.text_input("Zadej kód z mailu. / Enter the code:")
 
+        # Display the values of sent_code and entered_code
+        st.write("sent_code:", sent_code)
+        st.write("entered_code:", entered_code)
+
         # Validate the code
         #if entered_code is not None:
-        if validate_code(entered_code, school_name):
+        if validate_code(sent_code, entered_code):
             st.success("Code validated! You can now query the files.")
 
             # Check if the job is equal to "director"
             if job.lower() != "director":
+                # Let non/priviledged user query himself
                 if st.button("Ukaž moje podrobnosti. / Show my data."):
                     show_certain_columns(bucket_name, file_name, name, first_name, show_result=True)
-
+                
+            # Perform file querying for the director
             else:
-                # Perform file querying for the director
+                # Perform query for specific froms 
                 if st.button("Vypocitej P1c01_23"):
                     P1c01_23_count_people_by_department_stupen_trida(bucket_name, file_name, department, show_result=True)
-                if st.button("Vyhledej cloveka"):
-                    looked_name = st.text_input("Příjmení: / Name:")
-                    looked_first_name = st.text_input("Jméno: / First Name:")
-                    query_result = query_file(bucket_name, file_name, department, show_result=True)    
 
-                    if not validate_code (entered_code, school_name):
+                # Let director query all employees
+                if st.button("Hledat cloveka. / Find employee."):
+                    # Let director insert name of employees.
+                    name_and_surname = st.text_input("Hledane Příjmení: / Name:")
+
+                    # Split the name and first name
+                    split_names(name_and_surname)
+
+                    # Search the person in the file
+                    #first_name = st.text_input("Hledane Jméno: / First Name:")
+                    if st.button("Vyhledej cloveka"):
+                        query_someone(bucket_name, file_name, looked_name, looked_first_name, show_result=True)    
+
+                    if not validate_code (sent_code, entered_code):
                         st.warning("Invalid code. Please try again.")
-
+        else:
+            st.warning("Invalid code. Please try again.")
 
 if __name__ == '__main__':
     main()
