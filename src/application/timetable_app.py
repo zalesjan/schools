e0176801a6d3487a0736979685d4fef39d32a58b
Jan Zalesak <zalesjan@gmail.com>
2023-08-23 19:40:16 +0200

    Trying to incorporate Timetable as a separate app

--- src/application/timetable_app.py
+++ src/application/timetable_app.py
@@ -4,7 +4,10 @@ from modules.query_file import display_timetable
 def main():
     st.title("Timetable")
     st.write("This is the timetable page.")
-
+    
+    looked_first_name = "John"
+    looked_name = "Doe"
+    available_counts = {"Učím": 10, "Nepřímá": 8, "Z domu": 20, "Dozor": 20, "Oběd": 20}
     display_timetable(looked_first_name, looked_name, available_counts)
 
 if __name__ == '__main__':
