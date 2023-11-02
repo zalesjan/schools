503b9d705f33952b7d682fcb995177f44d5d4994
Jan Zalesak <zalesjan@gmail.com>
2023-11-02 13:49:23 +0100

    update of lesson times, days, PLUS NEW select_indirect_activity function

--- src/application/timetable_app.py
+++ src/application/timetable_app.py
@@ -1,7 +1,10 @@
 import streamlit as st
-from modules.query_file import display_timetable, select_indirect_activity
+from modules.query_file import display_timetable
 
 def main():
+    days_of_week = ['Pondělí', 'Úterý', 'Středa', 'Čtvrtek', 'Pátek']
+    # Unused days 'Pondělí / Monday', 'Úterý / Tuesday', 'Středa / Wednesday', 'Čtvrtek / Thursday', 'Pátek / Friday'
+
     st.title("Rozvrh zaměstnance ZŠ Masarova")
     st.write("Rozvrh slouží k evidenci pracovní doby zaměstnanců")
     
@@ -13,8 +16,18 @@ def main():
     selected_times = {}
     
     display_timetable(available_counts, selected_times)
-    select_indirect_activity(available_counts, selected_times)
-    
+
+    # Loop through each day of the week
+    for day in days_of_week:
+        add_time_range(day, selected_times)
+
+    # Display the selected time ranges
+    st.write("Selected Time Ranges:")
+    st.write(selected_times)
+
+    #Add a button to dynamically add more time ranges
+    if st.button("Add Time Range"):
+        new_day = st.selectbox("Select Day for the new time range:", days_of_week)    
   
 
 if __name__ == '__main__':
